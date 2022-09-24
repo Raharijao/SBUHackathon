abff0a8f8bb0c9266537c77e9854bfb34964f5ca
Raharijao <78986606+Raharijao@users.noreply.github.com>
2022-09-24 14:16:47 -0400

    Add files via upload

---
+++ 1 - Basic platformer/code/settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                                                        ',
+'                                                        ',
+'                                                        ',
+' XX    XXX            XX                                ',
+' XX P                                                   ',
+' XXXX         XX         XX                             ',
+' XXXX       XX                                          ',
+' XX    X  XXXX    XX  XXXXXX                                ',
+'       X  XXXX    XX  XXXXXXX                               ',
+'    XXXX  XXXXXX  XX  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX        ',
+'XXXXXXXX  XXXXXX  XX  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX              ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
